e2aea4d321b5368eba8daad0341dd7ec73af989f
lima3092 <lima3092@gmail.com>
2017-02-27 21:16:06 +0100

    add validation

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,2 +1,6 @@
 class Application < ApplicationRecord
+  validates :name,  :email, :level,  presence: true
+  validates :email, uniqueness: true
+  # validates :email, format: {with: @ , message}
+   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 end
