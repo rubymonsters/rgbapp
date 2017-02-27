06afb94c1d370ebae08c1ff5e958bc4e515bcda5
lima3092 <lima3092@gmail.com>
2017-02-27 21:51:29 +0100

    add language validation

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,4 +3,13 @@ class Application < ApplicationRecord
   validates :email, uniqueness: true
   # validates :email, format: {with: @ , message}
    validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+   validates :os, presence: true, unless: :needs_computer?
+   validate :at_least_select_one_language
+
+   def at_least_select_one_language
+     unless language_de? || language_en?
+       errors.add(:language_de, "please select at least one language")
+     end
+   end
+
 end
