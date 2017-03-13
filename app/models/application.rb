38f948b1ccb34436a103bd1d1c1711b225f0d94c
Uschi760 <seminararbeiten@gmx.de>
2017-03-13 20:29:48 +0100

    email validation message. fixes #7

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,8 +2,7 @@ class Application < ApplicationRecord
   validates :female, acceptance: true
   validates :name,  :email, :level,  presence: true
   validates :email, uniqueness: true
-  # validates :email, format: {with: @ , message}
-
+  validates :email, email: { message: "is not valid" }
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
   validates :os, presence: true, unless: :needs_computer?
   validate :at_least_select_one_language
