a4c2fa46eb9cae21ff7e689ff62113ce50522d27
Cathrine Dahl <cathrine.dahl@gmail.com>
2017-09-18 23:07:33 +0200

    Modify application test in test models

--- test/controllers/applications_controller_test.rb
+++ test/controllers/applications_controller_test.rb
@@ -1,14 +1,9 @@
 require 'test_helper'
 
 class ApplicationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get applications_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get applications_create_url
-    assert_response :success
-  end
-
+  # test "Get an event that does not exist responds to 404" do
+  #   e = Event.create
+  #   get "/events/5/applications/new"
+  #   assert_response 404
+  # end
 end
--- test/models/application_test.rb
+++ test/models/application_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class ApplicationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Cannot save unless one language is selected" do
+    a = Application.new(language_en: false, language_de: false)
+    a.save
+    assert_equal ["Please select at least one language."], a.errors[:language]
+  end
+
+  test "Save if one language is selected" do
+    a = Application.new(language_en: true, language_de: false)
+    a.save
+    assert_equal [], a.errors[:language]
+  end
+
+  test "Save if both languguages are saved" do
+    a = Application.new(language_en: true, language_de: true)
+    a.save
+    assert_equal [], a.errors[:language]
+  end
 end
