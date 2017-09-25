326722227a598ad65b967a8138db8cdfa027ea07
juopmu <juopmu@gmail.com>
2017-09-25 21:12:25 +0200

    Add further tests.

--- test/controllers/applications_controller_test.rb
+++ test/controllers/applications_controller_test.rb
@@ -1,9 +1,30 @@
 require 'test_helper'
 
 class ApplicationsControllerTest < ActionDispatch::IntegrationTest
-  # test "Get an event that does not exist responds to 404" do
-  #   e = Event.create
-  #   get "/events/5/applications/new"
-  #   assert_response 404
-  # end
+  test "Get an event that does not exist responds to 404" do
+    assert_raises ActiveRecord::RecordNotFound do
+      get "/events/5/applications/new"
+    end
+  end
+
+  test "Get an event that exists respond to 200" do
+    event = Event.create(name: "Test Me", place: "Testing", scheduled_at: "2017-09-25", application_start: Time.now, application_end: Time.now, confirmation_date: Time.now)
+    get "/events/#{event.id}/applications/new"
+    assert_response 200
+    assert_select "h1", "Test Me 25.09.2017"
+  end
+
+  test "Trying to apply without providing all the requested data." do
+    event = Event.create(name: "Test Me", place: "Testing", scheduled_at: Time.now, application_start: Time.now, application_end: Time.now, confirmation_date: Time.now)
+    post "/events/#{event.id}/applications", params: { application: {name: ""} }
+    assert_response 200
+    assert_select "h2", "Please check fields marked in red:"
+  end
+
+  test "Applying successfully." do
+    event = Event.create(name: "Test Me", place: "Testing", scheduled_at: Time.now, application_start: Time.now, application_end: Time.now, confirmation_date: Time.now)
+    post "/events/#{event.id}/applications", params: { application: {female: 1, name: "Test Applicant", email: "some@test.de", level: "3", os: "Mac", language_en: 1, read_coc: 1} }
+    assert_response 200
+    assert_select "p", "Thank you for filling out the form!"
+  end
 end
