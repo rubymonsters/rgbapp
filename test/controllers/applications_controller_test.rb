c5dfb4673bb3b839f10267b6cfcd5fd7bbf19adc
juopmu <juopmu@gmail.com>
2017-10-09 21:57:21 +0200

    Introduce factories in tests

--- test/controllers/applications_controller_test.rb
+++ test/controllers/applications_controller_test.rb
@@ -8,21 +8,21 @@ class ApplicationsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "Get an event that exists respond to 200" do
-    event = Event.create(name: "Test Me", place: "Testing", scheduled_at: "2017-09-25", application_start: Time.now, application_end: Time.now, confirmation_date: Time.now)
+    event = create(:event, name: "Test Me", scheduled_at: "2017-09-25")
     get "/events/#{event.id}/applications/new"
     assert_response 200
     assert_select "h1", "Test Me 25.09.2017"
   end
 
   test "Trying to apply without providing all the requested data." do
-    event = Event.create(name: "Test Me", place: "Testing", scheduled_at: Time.now, application_start: Time.now, application_end: Time.now, confirmation_date: Time.now)
+    event = create(:event)
     post "/events/#{event.id}/applications", params: { application: {name: ""} }
     assert_response 200
     assert_select "h2", "Please check fields marked in red:"
   end
 
   test "Applying successfully." do
-    event = Event.create(name: "Test Me", place: "Testing", scheduled_at: Time.now, application_start: Time.now, application_end: Time.now, confirmation_date: Time.now)
+    event = create(:event)
     post "/events/#{event.id}/applications", params: { application: {female: 1, name: "Test Applicant", email: "some@test.de", level: "3", os: "Mac", language_en: 1, read_coc: 1} }
     assert_response 200
     assert_select "p", "Thank you for filling out the form!"
--- test/system/select_applicants_test.rb
+++ test/system/select_applicants_test.rb
@@ -2,10 +2,11 @@ require "application_system_test_case"
 
 class SelectApplicantsTest < ApplicationSystemTestCase
   test "select applicant" do
-    event = Event.create(name: "Test Me", place: "Testing", scheduled_at: "2017-09-25", application_start: Time.now, application_end: Time.now, confirmation_date: Time.now)
-    user = User.create!(email: "test@user.de", password: "test", admin: true)
-    applicant1 = event.applications.create!(female: true, name: "Test Applicant1", email: "applicant1@test.de", level: "0", os: "Mac", language_en: true, read_coc: true)
-    applicant2 = event.applications.create!(female: true, name: "Test Applicant2", email: "applicant2@test.de", level: "0", os: "Windows", language_en: true, read_coc: true)
+    event = create(:event)
+    user = create(:user, email: "test@user.de", password: "test", admin: true)
+    #applicant1 = event.applications.create!(female: true, name: "Test Applicant1", email: "applicant1@test.de", level: "0", os: "Mac", language_en: true, read_coc: true)
+    applicant1 = create(:application, event: event)
+    applicant2 = create(:application, event: event)
 
     visit event_applications_path(event.id)
 
@@ -32,6 +33,5 @@ class SelectApplicantsTest < ApplicationSystemTestCase
     assert !applicant1.reload.selected?
     assert !applicant2.reload.selected?
 
-    #save_and_open_page
   end
 end
