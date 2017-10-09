a152eac184cd7e88fc24d9f9b30aaafd9da3743f
juopmu <juopmu@gmail.com>
2017-10-09 22:00:44 +0200

    Delete commented line

--- test/system/select_applicants_test.rb
+++ test/system/select_applicants_test.rb
@@ -4,7 +4,6 @@ class SelectApplicantsTest < ApplicationSystemTestCase
   test "select applicant" do
     event = create(:event)
     user = create(:user, email: "test@user.de", password: "test", admin: true)
-    #applicant1 = event.applications.create!(female: true, name: "Test Applicant1", email: "applicant1@test.de", level: "0", os: "Mac", language_en: true, read_coc: true)
     applicant1 = create(:application, event: event)
     applicant2 = create(:application, event: event)
 
