4f6561e68579b57cd614b84721fbe67711bc02e4
Thuy Le <inthuytion@gmail.com>
2017-10-30 20:42:01 +0000

    Feature: Limit application period
    - Add templates and partial based on application period
    - Add test cases for application start and end

--- test/controllers/applications_controller_test.rb
+++ test/controllers/applications_controller_test.rb
@@ -8,10 +8,24 @@ class ApplicationsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "Get an event that exists respond to 200" do
-    event = Event.create(name: "Test Me", place: "Testing", scheduled_at: "2017-09-25", application_start: Time.now, application_end: Time.now, confirmation_date: Time.now)
+    event = Event.create(name: "Test Me", place: "Testing", scheduled_at: "2017-09-25", application_start: Time.now, application_end: 10.days.from_now, confirmation_date: Time.now)
     get "/events/#{event.id}/applications/new"
     assert_response 200
     assert_select "h1", "Test Me 25.09.2017"
+  end
+
+  test "Get an event before application starts" do
+    event = Event.create(name: "Test Me", place: "Testing", scheduled_at: "2017-09-25", application_start: 2.days.from_now, application_end: 10.days.from_now, confirmation_date: 5.days.from_now)
+    get "/events/#{event.id}/applications/new"
+    assert_response 200
+    assert_select "TOO EARLY! THE APPLICATION HASN'T STARTED YET!"
+  end
+
+  test "Get an event after application ends" do
+    event = Event.create(name: "Test Me", place: "Testing", scheduled_at: "2017-09-25", application_start: 10.days.ago, application_end: 1.day.ago, confirmation_date: 5.days.from_now)
+    get "/events/#{event.id}/applications/new"
+    assert_response 200
+    assert_select "TOO LATE! THE APPLICATION TIME IS OVER!"
   end
 
   test "Trying to apply without providing all the requested data." do
