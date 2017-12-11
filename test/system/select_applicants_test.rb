7f6e82baf26d94f088ccd54634aec5eee04d7009
juopmu <juopmu@gmail.com>
2017-12-11 20:04:19 +0100

    Replace button by message when selection complete.

--- test/system/select_applicants_test.rb
+++ test/system/select_applicants_test.rb
@@ -1,36 +1,48 @@
 require "application_system_test_case"
 
 class SelectApplicantsTest < ApplicationSystemTestCase
-  test "select applicant" do
-    event = create(:event)
-    user = create(:user, email: "test@user.de", password: "test", admin: true)
-    applicant1 = create(:application, event: event)
-    applicant2 = create(:application, event: event)
 
-    visit event_applications_path(event.id)
+  setup do
+    @event = create(:event)
+    @user = create(:user, email: "test@user.de", password: "test", admin: true)
+    @applicant1 = create(:application, event: @event)
+    @applicant2 = create(:application, event: @event)
+
+    visit event_applications_path(@event.id)
 
     fill_in "Email", with: "test@user.de"
     fill_in "Password", with: "test"
 
     click_on "Sign in"
+  end
 
-    check("select_applicant_#{applicant1.id}")
-
-    click_on "Save"
-
-    assert_text "Cool! Changes saved."
-
-    assert applicant1.reload.selected?
-    assert !applicant2.reload.selected?
-
-    uncheck("select_applicant_#{applicant1.id}")
+  test "select applicant" do
+    check("select_applicant_#{@applicant1.id}")
 
     click_on "Save"
 
     assert_text "Cool! Changes saved."
 
-    assert !applicant1.reload.selected?
-    assert !applicant2.reload.selected?
+    assert @applicant1.reload.selected?
+    assert !@applicant2.reload.selected?
 
+    uncheck("select_applicant_#{@applicant1.id}")
+
+    click_on "Save"
+
+    assert_text "Cool! Changes saved."
+
+    assert !@applicant1.reload.selected?
+    assert !@applicant2.reload.selected?
+  end
+
+  test "complete selection" do
+    assert_no_text "The selection is completed"
+
+    click_on "Selection complete"
+
+    assert_text "The selection is completed"
+
+    assert has_no_button?('Selection complete')
   end
 end
