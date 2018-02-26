e6b3e448172e6864a2df4e16be47820f8db79bb7
juopmu <juopmu@gmail.com>
2018-02-26 20:16:14 +0100

    Add tests for reminders

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,13 @@
 class Event < ApplicationRecord
   has_many :applications
 
-  validates :name, :place, :scheduled_at, :application_start, :application_end, :confirmation_date, :start_time, :end_time, presence: true 
+  validates :name, :place, :scheduled_at, :application_start, :application_end, :confirmation_date, :start_time, :end_time, presence: true
+
+  def self.send_reminders
+    Event.where(scheduled_at: 2.days.from_now.to_date.all_day).each do |event|
+      event.applications.where(selected: true, attendance_confirmed: true). each do |application|
+        UserMailer.reminder_mail(application).deliver_now
+      end
+    end
+  end
 end
--- lib/tasks/reminders.rake
+++ lib/tasks/reminders.rake
@@ -1,7 +1,3 @@
 task :send_reminders => :environment do
-  Event.where(scheduled_at: 2.days.from_now.to_date.all_day).each do |event|
-    event.applications.where(selected: true, attendance_confirmed: true). each do |application|
-      UserMailer.reminder_mail(application).deliver_later
-    end
-  end
+  Event.send_reminders
 end
