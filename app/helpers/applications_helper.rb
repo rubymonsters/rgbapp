289bf1662880d98291cc584eac7ccf03467a7ceb
juopmu <juopmu@gmail.com>
2018-03-19 21:38:01 +0100

    Allow admins to edit events

--- app/helpers/applications_helper.rb
+++ app/helpers/applications_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationsHelper
 
-def error_message(field)
-  @application.errors[field].map do |error|
+def error_message(object, field)
+  object.errors[field].map do |error|
   "<p class=\"error_message\"> #{error} </p>"
 end.join.html_safe
 end
