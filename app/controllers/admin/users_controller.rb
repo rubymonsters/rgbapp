bb357e6b7986728899c555283e58788c585e9e76
juopmu <juopmu@gmail.com>
2018-05-14 21:29:11 +0200

    Add table of users and button to make user an admin fixes #77

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,19 @@
+class Admin::UsersController < ApplicationController
+
+  before_action :require_admin
+
+  def index
+    @users = User.all
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user == current_user
+      flash[:error] = "You cannot change the logged in user."
+    else
+      @user.admin = !@user.admin
+      @user.save
+    end
+    redirect_to admin_users_path
+  end
+end
