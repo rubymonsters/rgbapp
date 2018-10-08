712c0a8bbbc30c828a50fffd850a1500dd99398f
juopmu <juopmu@gmail.com>
2018-10-08 22:10:02 +0200

    Show real event data in e-mail preview fixes #116

--- app/helpers/admin/templates_helper.rb
+++ app/helpers/admin/templates_helper.rb
@@ -1,37 +1,39 @@
 module Admin::TemplatesHelper
 
-ExampleData = {
-  application_mail: {
-    applicant_name: "Ruby",
-    event_date: "01.01.3000",
-    selection_email_deadline: "31.12.2999"
-  },
-  selection_mail: {
-    applicant_name: "Ruby",
-    event_date: "01.01.3000",
-    confirmation_deadline: "31.12.2999",
-    confirmation_link: "http://www.exampleconfirmationlink.com"
-  },
-  rejection_mail: {
-    applicant_name: "Ruby"
-  },
-  reminder_mail: {
-    applicant_name: "Ruby",
-    event_date: "01.01.3000",
-    event_place: "Wakanda"
-  },
-  waiting_list_mail: {
-    applicant_name: "Ruby",
-    event_date: "01.01.3000",
-  }
-}
+  def example_data
+    {
+      application_mail: {
+        applicant_name: "Ruby",
+        event_date: @event.scheduled_at.strftime("%d.%m.%Y"),
+        selection_email_deadline: @event.confirmation_date.strftime("%d.%m.%Y")
+      },
+      selection_mail: {
+        applicant_name: "Ruby",
+        event_date: @event.scheduled_at.strftime("%d.%m.%Y"),
+        confirmation_deadline: (@event.confirmation_date + 5.days).strftime("%d.%m.%Y"),
+        confirmation_link: event_application_confirm_url(event_id: @event.id, application_id: SecureRandom.hex(12))
+      },
+      rejection_mail: {
+        applicant_name: "Ruby"
+      },
+      reminder_mail: {
+        applicant_name: "Ruby",
+        event_date: @event.scheduled_at.strftime("%d.%m.%Y"),
+        event_place: @event.place
+      },
+      waiting_list_mail: {
+        applicant_name: "Ruby",
+        event_date: @event.scheduled_at.strftime("%d.%m.%Y"),
+      }
+    }
+  end
 
   def preview_template(template_name)
     begin
-      Mustache.render(@event[template_name], ExampleData[template_name.sub(/_subject$/,"").to_sym]
-      )
-    rescue => e
-      "Error: #{e.message}"
-    end
+      Mustache.render(@event[template_name], example_data[template_name.sub(/_subject$/,"").to_sym]
+    )
+  rescue => e
+    "Error: #{e.message}"
   end
+end
 end
