6657a0e81e69c11faf516b88d1c2dbe0a6c8a04a
juopmu <juopmu@gmail.com>
2019-02-04 20:26:28 +0100

    Show errors when checking attendants

--- app/assets/javascripts/admin/events.js
+++ app/assets/javascripts/admin/events.js
@@ -1,19 +1,16 @@
 $(document).ready(function() {
   $(".attended").click(function() {
+    var checkbox = $(this)
     $.ajax({
-      url: $(this).data( "url" ), 
+      url: $(this).data( "url" ),
       method: 'PUT',
       data: JSON.stringify({attendant:{ attended: $(this).prop('checked') }}),
       contentType: "application/json; charset=utf-8",
       dataType: "json",
+      error: function(_, _, _){
+        checkbox.prop('checked', !checkbox.prop('checked'));
+        alert("Oops, something went wrong. Please check your Internet connection.")
+      },
     })
   });
 });
-
-// $(document).ready(function() {
-// 	$(".attended").click(function() {
-// 		console.log("The checkbox for the attendant number was checked or unchecked");
-// 		console.log($(this).prop('checked'));
-// 		console.log($(this).attr('data-id'));
-// 	});
-// });
