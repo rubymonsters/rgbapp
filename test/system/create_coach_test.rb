d6dab3fceefa954d0afec2dc57d974ad994d7441
juopmu <juopmu@gmail.com>
2019-11-04 20:17:48 +0100

    Add create coach test

---
+++ test/system/create_coach_test.rb
@@ -0,0 +1,28 @@
+require "application_system_test_case"
+
+class CreateCoachTest < ApplicationSystemTestCase
+
+  test "registration" do
+    visit "/coaches/new"
+
+    fill_in "E-mail address", with: "test@coach.de"
+    fill_in "Password", with: "password"
+    fill_in "Name", with: "Super coach"
+    check "female/I have some female sex characteristics."
+    check "English"
+    check "Notifications about upcoming events"
+
+    click_on "Submit"
+
+    coach = Coach.last
+
+    assert_equal coach.name, "Super coach"
+    assert_equal coach.user.email, "test@coach.de"
+    assert_equal coach.female, true
+    assert_equal coach.language_en, true
+    assert_equal coach.language_de, false
+    assert_equal coach.notifications, true
+
+  end
+
+end
