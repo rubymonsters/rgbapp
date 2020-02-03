c2218e9d443d978f46dda1f58014732c1040a423
juopmu <juopmu@gmail.com>
2020-02-03 19:41:23 +0100

    Add test for sending coach approval emails

--- test/system/select_coaches_test.rb
+++ test/system/select_coaches_test.rb
@@ -2,11 +2,12 @@ require "application_system_test_case"
 
 class SelectCoachesTest < ApplicationSystemTestCase
   setup do
-    @event = create(:event, start_time: "09:00", end_time: "17:30")
+    @event = create(:event, place: "Cool Office", start_time: "09:00", end_time: "17:30", coach_approval_mail_subject: "You are approved",
+      coach_approval_mail: "Welcome {{ coach_name }} to the workshop on {{ event_date }} at {{ event_place }}")
     @admin = create(:user, email: "test@user.de", password: "test", admin: true)
     @user1 = create(:user, email: "user1@coach.de", password: "password")
     @user2 = create(:user, email: "user2@coach.de", password: "password")
-    @coach1 = create(:coach, user: @user1)
+    @coach1 = create(:coach, user: @user1, name: "Supercoach")
     @coach2 = create(:coach, user: @user2)
     @coach_application1 = create(:coach_application, event: @event, coach: @coach1, lightningtalk: "Amazing talk")
     @coach_application2 = create(:coach_application, event: @event, coach: @coach2)
@@ -43,5 +44,24 @@ class SelectCoachesTest < ApplicationSystemTestCase
     assert_text "Cool! Changes saved."
 
     assert @coach_application1.reload.rejected?
+  end
+
+  test "Send approval e-mail" do
+    select("Approved", from: "state_#{@coach_application1.id}")
+
+    click_on "Save"
+
+    clear_emails
+
+    perform_enqueued_jobs do
+      click_on "Send 1 e-mail"
+    end
+
+    open_email(@coach_application1.coach.user.email)
+
+    assert_equal current_email.subject, "You are approved"
+    assert current_email.has_content?("Welcome Supercoach to the workshop")
+    assert current_email.has_content?("on #{@event.scheduled_at.strftime("%d.%m.%Y")}")
+    assert current_email.has_content?("at Cool Office")
   end
 end
