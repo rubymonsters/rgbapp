9ff62b651cc2808cb946b17ac63d60d57e3928c2
Astrid Varga <astrid.varga@gmx.de>
2020-02-10 20:54:45 +0100

    add first time coaching column to coach applications table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_190432) do
+ActiveRecord::Schema.define(version: 2020_02_10_194758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2020_02_10_190432) do
     t.integer "state", default: 0, null: false
     t.boolean "lightningtalk_approved", default: false
     t.datetime "contacted_at"
+    t.boolean "first_time_coaching"
     t.index ["coach_id"], name: "index_coach_applications_on_coach_id"
     t.index ["event_id"], name: "index_coach_applications_on_event_id"
   end
