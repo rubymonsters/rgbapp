102fbffa3592dbd38f07bbaec5a2789852794a7b
Astrid Varga <astrid.varga@gmx.de>
2020-04-23 19:33:41 +0200

    start groups table

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -7,6 +7,30 @@ class Admin::GroupsController < ApplicationController
   end
 
   def generate
+    # 50 Teilnehmer insgesamt
+    # mach 10 Gruppen á 5 Teilnehmer
+    # mach für jede Gruppe einen DB Eintrag mit name = "Group " + index Zahl 
+
+    # (Anzahl der Gruppen = Anzahl der Attendees, die attended haben / 6 )
+    #p application.attended
+    #
+    # if @event.applications.attended && @event.applications.language_de 
+    # attendants = @event.applications.attendants
+
+    @attendants = @event.applications.application_selected.confirmed
+    puts ('huhuu')
+    puts @attendants
+    
+    # attendants.each do |attendant|
+    #   puts ('huhuuu')
+    #   puts @attendant
+    # end
+    puts @event.applications #.attendants 
+      #attended = 'true')
+    # end
+    # Gruppennummer zuordnen put application_id in event_group_attendees
+    # 
+
     redirect_to admin_event_groups_path(@event), notice: "Groups successfully generated"
   end
 
