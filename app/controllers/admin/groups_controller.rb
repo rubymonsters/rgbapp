f9584e08ebf43a151daeaf614e4001811c34f8d6
Astrid Varga <astrid.varga@gmx.de>
2020-05-03 19:23:31 +0200

    Add coach names to event groups view and restructure logic of group creation

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -8,21 +8,36 @@ class Admin::GroupsController < ApplicationController
   end
 
   def generate
-    # This needs to be wrapped in a transaction
-    @attendees = @event.applications.application_selected.confirmed
-    @attendees.each_slice(6).with_index do |group, index|
+
+    @coaches = @event.coach_applications.approved.to_a
+    @coaches.each_slice(2).with_index do |group, index|
       event_group = EventGroup.create(event: @event, name: "Group #{index + 1}")
-      group.each do |application|
-        event_group.applications << application
+      group.each do |coach_application|
+        event_group.coach_applications << coach_application
       end
     end
 
-    @coaches = @event.coach_applications.approved.to_a
+    @attendees = @event.applications.application_selected.confirmed
     @event.event_groups.each do |event_group|
-      # Check if we are assinging the last 2 coaches
-      coach_group = @coaches.pop(2)
-      event_group.coach_applications << coach_group unless coach_group.empty?
+      attendee_group = @attendees.pop()
+      event_group.applications << attendee_group unless attendee_group.empty?
     end
+
+    # This needs to be wrapped in a transaction
+    # @attendees = @event.applications.application_selected.confirmed
+    # @attendees.each_slice(6).with_index do |group, index|
+    #   event_group = EventGroup.create(event: @event, name: "Group #{index + 1}")
+    #   group.each do |application|
+    #     event_group.applications << application
+    #   end
+    # end
+
+    # @coaches = @event.coach_applications.approved.to_a
+    # @event.event_groups.each do |event_group|
+    #   # Check if we are assinging the last 2 coaches
+    #   coach_group = @coaches.pop(2)
+    #   event_group.coach_applications << coach_group unless coach_group.empty?
+    # end
 
     @event_groups = @event.event_groups
 
