053061de2f59841f6f0b54b6a5d5fd6a15672ce6
Astrid Varga <astrid.varga@gmx.de>
2020-05-08 00:24:38 +0200

    fix attendee distribution to event groups

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -9,6 +9,7 @@ class Admin::GroupsController < ApplicationController
 
   def generate
 
+    # create a group for each pair of coaches
     @coaches = @event.coach_applications.approved.to_a
     @coaches.each_slice(2).with_index do |group, index|
       event_group = EventGroup.create(event: @event, name: "Group #{index + 1}")
@@ -17,27 +18,14 @@ class Admin::GroupsController < ApplicationController
       end
     end
 
-    @attendees = @event.applications.application_selected.confirmed
-    @event.event_groups.each do |event_group|
-      attendee_group = @attendees.pop()
-      event_group.applications << attendee_group unless attendee_group.empty?
+    # add one attendee to each group as long as there are attendees in array
+    @attendees = @event.applications.application_selected.confirmed.to_a
+    @attendees.each do |attendee|
+      @event.event_groups.each do |event_group|
+        attendee_group = @attendees.pop(1)
+        event_group.applications << attendee_group unless attendee_group.empty?
+      end
     end
-
-    # This needs to be wrapped in a transaction
-    # @attendees = @event.applications.application_selected.confirmed
-    # @attendees.each_slice(6).with_index do |group, index|
-    #   event_group = EventGroup.create(event: @event, name: "Group #{index + 1}")
-    #   group.each do |application|
-    #     event_group.applications << application
-    #   end
-    # end
-
-    # @coaches = @event.coach_applications.approved.to_a
-    # @event.event_groups.each do |event_group|
-    #   # Check if we are assinging the last 2 coaches
-    #   coach_group = @coaches.pop(2)
-    #   event_group.coach_applications << coach_group unless coach_group.empty?
-    # end
 
     @event_groups = @event.event_groups
 
